ecc377a7dd4cced2d8f37d45f6c4d385c31f9420
naurias <dev.earmark338@passinbox.com>
2026-09-08 07:45:50 +0500

    version bump

--- flake.nix
+++ flake.nix
@@ -14,12 +14,12 @@
       {
         packages.default = pkgs.buildGoModule {
           pname = "liber";
-          version = "0.7.1";
+          version = "0.8.1";
           src = ./.;
 
           vendorHash = null;
 
-          ldflags = [ "-X main.Version=0.7.1" ];
+          ldflags = [ "-X main.Version=0.8.1" ];
           buildInputs = [ pkgs.fzf pkgs.single-file-cli pkgs.monolith];
 
 
